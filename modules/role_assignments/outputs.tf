3dc0dabe8492ef0e5a061589e061c727a3575b5b
kmuthuku-ic <kmuthuku@ic.ac.uk>
2026-07-15 19:00:06 +0530

    corrected role assignment output.tf

--- modules/role_assignments/outputs.tf
+++ modules/role_assignments/outputs.tf
@@ -1,11 +1,20 @@
-output "storage_blob_role_assignment_id" {
-  value = azurerm_role_assignment.storage_blob_data_contributor.id
+output "storage_blob_role_assignment_ids" {
+  value = {
+    for key, role in azurerm_role_assignment.storage_blob_data_contributor :
+    key => role.id
+  }
 }
 
-output "key_vault_role_assignment_id" {
-  value = azurerm_role_assignment.key_vault_secrets_user.id
+output "key_vault_role_assignment_ids" {
+  value = {
+    for key, role in azurerm_role_assignment.key_vault_secrets_user :
+    key => role.id
+  }
 }
 
-output "monitoring_reader_role_assignment_id" {
-  value = azurerm_role_assignment.monitoring_reader.id
+output "monitoring_reader_role_assignment_ids" {
+  value = {
+    for key, role in azurerm_role_assignment.monitoring_reader :
+    key => role.id
+  }
 }
